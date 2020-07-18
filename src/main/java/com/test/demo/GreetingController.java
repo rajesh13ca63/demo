33abff79a75423c4cbfb5d47e848ec00d50260f1
Rajesh <rajeshkumargupta001@gmail.com>
2020-07-18 15:22:23 +0530

    Added new hello api for testing

--- src/main/java/com/test/demo/GreetingController.java
+++ src/main/java/com/test/demo/GreetingController.java
@@ -3,7 +3,9 @@ package com.test.demo;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +15,13 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 	
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name ) {
+	public @ResponseBody Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name ) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello Spring World";
 	}
 	
 	
